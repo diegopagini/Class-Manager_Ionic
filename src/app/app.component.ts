d0e90f78254474faebbdf6be78ee1ec3e6b1a1ee
Diego Pagini <diego.pagini@neoris.com>
2023-11-13 09:46:17 +0100

    sqliteservice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { NgIf } from '@angular/common';
-import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import {
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { Device } from '@capacitor/device';
 import { IonApp, IonRouterOutlet, Platform } from '@ionic/angular/standalone';
 import { TranslateService } from '@ngx-translate/core';
 import { defineCustomElements as jeepSqlite } from 'jeep-sqlite/loader';
+import { Subject, takeUntil } from 'rxjs';
+
+import { SqliteManagerService } from './services/sqlite-manager.service';
 
 jeepSqlite(window);
 
@@ -14,16 +22,21 @@ jeepSqlite(window);
   imports: [IonApp, IonRouterOutlet, NgIf],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppComponent implements OnInit {
-  isWeb: boolean;
+export class AppComponent implements OnInit, OnDestroy {
+  isWeb = false;
+  load = false;
+  private unsubscribe$ = new Subject<void>();
 
   constructor(
     private platform: Platform,
+    private sqliteService: SqliteManagerService,
     private translateService: TranslateService
   ) {}
+
   ngOnInit(): void {
     this.setLang();
     this.setIsWeb();
+    this.initDb();
   }
 
   private setLang(): void {
@@ -38,5 +51,19 @@ export class AppComponent implements OnInit {
   private async setIsWeb(): Promise<void> {
     const info = await Device.getInfo();
     this.isWeb = info.platform === 'web';
+  }
+
+  private initDb(): void {
+    this.sqliteService.init();
+    this.sqliteService.dbReady$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe((isReady: boolean) => {
+        this.load = isReady;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
---
+++ src/app/services/sqlite-manager.service.ts
@@ -0,0 +1,95 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CapacitorSQLite, JsonSQLite } from '@capacitor-community/sqlite';
+import { Device } from '@capacitor/device';
+import { Preferences } from '@capacitor/preferences';
+import { AlertController } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SqliteManagerService {
+  dbReady$ = new BehaviorSubject<boolean>(false);
+  private DB_NAME_KEY = 'db_name';
+  private DB_SETUP_KEY = 'first_db_setup';
+  private dbName: string;
+  private isWeb: boolean = false;
+
+  constructor(
+    private alertController: AlertController,
+    private http: HttpClient
+  ) {}
+
+  async init(): Promise<void> {
+    const info = await Device.getInfo();
+    const sqlite = CapacitorSQLite as any;
+
+    if (info.platform === 'android') {
+      try {
+        await sqlite.requestPermissions();
+      } catch (error) {
+        const alert = await this.alertController.create({
+          header: 'Sin acceso a la base de datos',
+          message: 'Esta app no puede funcionar sin accesso a la base de datos',
+          buttons: ['OK'],
+        });
+
+        await alert.present();
+      }
+    } else if (info.platform === 'web') {
+      this.isWeb = true;
+      await sqlite.initWebStore();
+    }
+
+    this.setupDatabase();
+  }
+
+  async setupDatabase(): Promise<void> {
+    const dbSetupDone = await Preferences.get({
+      key: this.DB_SETUP_KEY,
+    });
+
+    if (!dbSetupDone.value) this.downloadDatabase();
+    else {
+      const dbName = await this.getDbName();
+      await CapacitorSQLite.createConnection({
+        database: dbName,
+      });
+      await CapacitorSQLite.open({
+        database: dbName,
+      });
+      this.dbReady$.next(true);
+    }
+  }
+
+  downloadDatabase(): void {
+    this.http.get('assets/db/db.json').subscribe(async (jsonExport) => {
+      const jsonstring = JSON.stringify(jsonExport);
+      const isValid = await CapacitorSQLite.isJsonValid({ jsonstring });
+
+      if (isValid.result) {
+        this.dbName = (jsonExport as JsonSQLite).database;
+        await CapacitorSQLite.importFromJson({ jsonstring });
+        await CapacitorSQLite.createConnection({
+          database: this.dbName,
+        });
+        await CapacitorSQLite.open({
+          database: this.dbName,
+        });
+        await Preferences.set({ key: this.DB_SETUP_KEY, value: '1' });
+        await Preferences.set({ key: this.DB_NAME_KEY, value: this.dbName });
+        this.dbReady$.next(true);
+      }
+    });
+  }
+
+  async getDbName(): Promise<string> {
+    if (!this.dbName) {
+      const dbName = await Preferences.get({ key: this.DB_NAME_KEY });
+      this.dbName = dbName.value as string;
+    }
+
+    return this.dbName;
+  }
+}
